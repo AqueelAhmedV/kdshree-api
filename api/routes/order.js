91bb535083f30307c1950ed8ef9949dc2c90b36a
Aqueel Ahmed V <aqueelvallanchira13@gmail.com>
2023-10-28 23:04:17 +0530

    add order/list route

--- api/routes/order.js
+++ api/routes/order.js
@@ -2,5 +2,6 @@ const router = require("express").Router()
 const orderFns = require("../../controllers/order")
 
 router.post("/place-order", orderFns.placeOrder)
+router.post("/list", orderFns.getOrdersBySeller)
 
 module.exports = router
